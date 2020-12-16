8be21130d44ca3868b008ba194f51024d0bddc66
Puck van Gerwen <3156622-puck_vg@users.noreply.gitlab.com>
2020-12-16 15:07:52 +0100

    formatted python

--- dummy.py
+++ dummy.py
@@ -1,66 +1,74 @@
-import time 
+import time
+
 
 def add_stuff():
     return 2 + 2
 
+
 def multiply_stuff():
-    return 2 * 2 
+    return 2 * 2
+
 
 def divide_stuff():
     return 5 / 2
 
+
 def subtract_stuff():
     return 5 - 2
 
+
 def short_sleep():
     time.sleep(0.1)
-    return 
+    return
+
 
 def medium_sleep():
     time.sleep(1)
-    return 
+    return
+
 
 def long_sleep():
     time.sleep(5)
     return
 
+
 def sleepy_day():
     short_sleep()
     medium_sleep()
     long_sleep()
-    return 
+    return
+
 
 def somewhat_productive_day():
     short_sleep()
     add_stuff()
     subtract_stuff()
     long_sleep()
-    return 
+    return
+
 
 def productive_day():
     multiply_stuff()
     divide_stuff()
     add_stuff()
     subtract_stuff()
-    return 
+    return
+
 
 def my_week():
 
-    # monday 
+    # monday
     sleepy_day()
     # tuesday
     somewhat_productive_day()
-    # wednesday 
+    # wednesday
     somewhat_productive_day()
-    # thursday 
+    # thursday
     productive_day()
     # friday
     sleepy_day()
-    return 
+    return
+
 
 if __name__ == "__main__":
     my_week()
-
-
-
-
--- qml_profile.py
+++ qml_profile.py
@@ -1,26 +1,26 @@
-import qml 
-from qml.math import cho_solve
+import numpy as np
+import qml
+import sklearn
 from qml.kernels import gaussian_kernel
-import sklearn 
-from sklearn.model_selection import train_test_split 
+from qml.math import cho_solve
 from sklearn.kernel_ridge import KernelRidge
-import numpy as np 
+from sklearn.model_selection import train_test_split
 
 
 def generate_dummy_data():
-    X = np.random.rand(100,100)
+    X = np.random.rand(100, 100)
     y = np.random.rand(100)
 
-    return X, y 
+    return X, y
 
 
 def compute_kernel_qml(X_i, X_j, sigma=1e3):
     return gaussian_kernel(X_i, X_j, sigma)
 
 
-def train_KRR_qml(X, y, sigma=1e3, llambda=1e-8): 
+def train_KRR_qml(X, y, sigma=1e3, llambda=1e-8):
     K = compute_kernel_qml(X, X, sigma=sigma)
-    K[np.diag_indices_from(K)] += llambda 
+    K[np.diag_indices_from(K)] += llambda
     alpha = cho_solve(K, y)
     return alpha
 
@@ -28,39 +28,39 @@ def train_KRR_qml(X, y, sigma=1e3, llambda=1e-8):
 def train_KRR_sklearn(X, y, sigma=1e-3, llambda=1e-8):
     clf = KernelRidge(alpha=llambda, kernel="rbf", gamma=sigma)
     clf.fit(X, y)
-    return clf 
+    return clf
 
 
-def predict_KRR_sklearn(X_test, y_test, X_train=None, y_train=None,
-        clf=None, sigma=1e3, llambda=1e-8):
-    if not clf: 
-        assert all(v is not None for v in [X_train, y_train]), "cannot train model without training data"
+def predict_KRR_sklearn(
+    X_test, y_test, X_train=None, y_train=None, clf=None, sigma=1e3, llambda=1e-8
+):
+    if not clf:
+        assert all(
+            v is not None for v in [X_train, y_train]
+        ), "cannot train model without training data"
         clf = train_KRR_sklearn(X_train, y_train, sigma=sigma, llambda=llambda)
     y_pred = clf.predict(X_test)
     return y_pred
 
 
-def predict_KRR_qml(X_test, y_test, X_train=None, y_train=None, alpha=None, 
-                    sigma=1e3, llambda=1e-8):
-    if not alpha: 
-        assert all(v is not None for v in [X_train, y_train]), "cannot train model without training data"
-        alpha = train_KRR_qml(X_train, y_train, sigma=sigma, llambda=\
-                llambda)
+def predict_KRR_qml(
+    X_test, y_test, X_train=None, y_train=None, alpha=None, sigma=1e3, llambda=1e-8
+):
+    if not alpha:
+        assert all(
+            v is not None for v in [X_train, y_train]
+        ), "cannot train model without training data"
+        alpha = train_KRR_qml(X_train, y_train, sigma=sigma, llambda=llambda)
     Ks = compute_kernel_qml(X_test, X_train, sigma)
     y_pred = np.dot(Ks, alpha)
 
-    return y_pred 
+    return y_pred
 
 
 if __name__ == "__main__":
     X, y = generate_dummy_data()
     X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.33, random_state=42)
+        X, y, test_size=0.33, random_state=42
+    )
     pred_qml = predict_KRR_qml(X_test, y_test, X_train=X_train, y_train=y_train)
     pred_sklearn = predict_KRR_sklearn(X_test, y_test, X_train=X_train, y_train=y_train)
-    
-
-
-
-
-
